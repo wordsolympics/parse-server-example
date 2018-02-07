f7c55cd964e5f28176a02a4f33912e00edf6c164
wordsolympics <35567101+wordsolympics@users.noreply.github.com>
2018-02-07 12:36:06 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,26 @@
 
+var GameManager = require('cloud/game_matching.js');
+
+
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
+});
+
+
+Parse.Cloud.define("joinNewGame", function(request, response) {
+  console.log("Incoming join request from " + request.user);
+  
+  if (request.user) {
+    GameManager.joinAnonymousGame(request.user, {
+      success: function(match, isTurn) {
+        response.success(match);
+      },
+      error: function(error) {
+        console.log(error);
+        response.error("An error has occured.");
+      }
+    });
+  } else {
+    response.error("Authentication failed");
+  }
 });
